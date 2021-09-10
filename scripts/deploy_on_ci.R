6c0e15cc96ecc589c4f421a240f4c4a8ee686ab5
Barret Schloerke <schloerke@gmail.com>
2021-09-10 16:53:19 -0400

    Rearrange code to print all apps and run all apps in parallel

--- scripts/deploy_on_ci.R
+++ scripts/deploy_on_ci.R
@@ -34,58 +34,62 @@ deploy_app <- function(
 
 
 # code_chunk(OSUICode::get_example("tabler/button"), "r")
-
-doParallel::registerDoParallel(cores = 3)
-
 pkg_root <-  rprojroot::find_package_root_file("")
-for (rmd in dir(pkg_root, pattern = "\\.Rmd$")) {
+app_list <- lapply(dir(pkg_root, pattern = "\\.Rmd$"), function(rmd) {
   rmd_lines <- readLines(rmd, warn = FALSE)
   is_example_code <- grepl("OSUICode::get_example", rmd_lines, fixed = TRUE)
-  if (any(is_example_code)) {
-    example_lines <- rmd_lines[is_example_code]
-    # collect app name like `"tabler/button"`
-    app_names <- sub('^.*OSUICode::get_example\\("([^"]+)".*$', "\\1", example_lines)
-    # collect package name used (if any) otherwise use `"OSUICode"`
-    package_names <- rep("OSUICode", length(app_names))
-    has_package <- grepl("package\\s*=", example_lines)
-    package_names[has_package] <- sub('^.*OSUICode::get_example\\(.*package\\s*=\\s*"([^"]+)".*$', "\\1", example_lines[has_package])
+  if (!any(is_example_code)) return(NULL)
+  example_lines <- rmd_lines[is_example_code]
+  # collect app name like `"tabler/button"`
+  app_names <- sub('^.*OSUICode::get_example\\("([^"]+)".*$', "\\1", example_lines)
+  # collect package name used (if any) otherwise use `"OSUICode"`
+  package_names <- rep("OSUICode", length(app_names))
+  has_package <- grepl("package\\s*=", example_lines)
+  package_names[has_package] <- sub('^.*OSUICode::get_example\\(.*package\\s*=\\s*"([^"]+)".*$', "\\1", example_lines[has_package])
 
-    plyr::m_ply(
-      .parallel = TRUE,
-      data.frame(
-        app_name = app_names,
-        package_name = package_names
-      ),
-      function(app_name, package_name) {
-        app_dir <- file.path(tempdir(), tempfile("OSUICode-app-"))
-        dir.create(app_dir, recursive = TRUE, showWarnings = FALSE)
-        on.exit({
-          unlink(app_dir, recursive = TRUE)
-        }, add = TRUE)
+  data.frame(
+    app_name = app_names,
+    package_name = package_names
+  )
+})
 
-        # Make app.R
-        cat(
-          file = file.path(app_dir, "app.R"),
-          "\nOSUICode::run_example(\"", app_name, "\", package = \"", package_name, "\")\n"
-        )
+apps <- do.call(rbind, app_list)
+message("Applications:")
+print(apps)
 
-        # Copy in DESCRIPTION to find package deps
-        file.copy(
-          file.path(pkg_root, "DESCRIPTION"),
-          file.path(app_dir, "DESCRIPTION")
-        )
+# Deploy in parallel
+doParallel::registerDoParallel(cores = 3)
+plyr::m_ply(
+  .parallel = TRUE,
+  apps,
+  function(app_name, package_name) {
+    app_dir <- file.path(tempdir(), tempfile("OSUICode-app-"))
+    dir.create(app_dir, recursive = TRUE, showWarnings = FALSE)
+    on.exit({
+      unlink(app_dir, recursive = TRUE)
+    }, add = TRUE)
 
-        # Deploy!
-        deploy_app(
-          app_dir = app_dir,
-          name = gsub("/", "_", app_name, fixed = TRUE),
-          package = package_name
-        )
-      }
+    # Make app.R
+    cat(
+      sep = "",
+      file = file.path(app_dir, "app.R"),
+      "\nOSUICode::run_example(\"", app_name, "\", package = \"", package_name, "\")\n"
     )
 
+    # Copy in DESCRIPTION to find package deps
+    file.copy(
+      file.path(pkg_root, "DESCRIPTION"),
+      file.path(app_dir, "DESCRIPTION")
+    )
+
+    # Deploy!
+    deploy_app(
+      app_dir = app_dir,
+      name = gsub("/", "_", app_name, fixed = TRUE),
+      package = package_name
+    )
   }
-}
+)
 
 
 message("done")
